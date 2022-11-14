5f76506c33e70c656a3061f84ee7ed5d4d512df4
malikbendevv <malikbendevv@gmail.com>
2022-11-14 18:29:24 +0100

    last-commit

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -4,8 +4,8 @@ import "./FooterOverlay.css";
 
 const FooterOverlay = () => (
   <div className="app__footerOverlay">
-    <div className="app.footerOverlay-black"></div>
-    <div className="app.footerOverlay-img app__bg"></div>
+    <div className="app__footerOverlay-black"></div>
+    <div className="app__footerOverlay-img app__bg"></div>
   </div>
 );
 
